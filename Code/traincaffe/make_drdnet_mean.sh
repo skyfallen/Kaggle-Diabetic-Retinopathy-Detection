f7dae2f2339ee7555b7b1a4bd1131a69067e1ef4
Anna Leontjeva <hpc_anna1985@juur.grid.eenet.ee>
2015-05-12 22:13:09 +0300

    script for mean extraction

--- Code/traincaffe/make_drdnet_mean.sh
+++ Code/traincaffe/make_drdnet_mean.sh
@@ -3,12 +3,12 @@
 # N.B. this is available in data/ilsvrc12
 
 PREPROC=size256
-LMDB=/storage/hpc_anna/Kaggle_DRD/sample_lmdb
-INPUT=/storage/hpc_anna/Kaggle_DRD/sample_input
+CAFFEINPUT=/storage/hpc_anna/Kaggle_DRD/sample_caffeinput
+IMAGES=/storage/hpc_anna/Kaggle_DRD/sample_images
 
 TOOLS=$HOME/Software/Caffe/build/tools
 
-$TOOLS/compute_image_mean $OUTPUT/$PREPROC/ilsvrc12_train_lmdb \
-  $INPUT/$PREPROC/drdnet_mean.binaryproto
+$TOOLS/compute_image_mean $CAFFEINPUT/$PREPROC/ilsvrc12_train_lmdb \
+  $CAFFEINPUT/$PREPROC/drdnet_mean.binaryproto
 
 echo "Done."
