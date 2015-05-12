913473d6b1698461d3ace1e78c0452526fcd5ead
Dmytro Fishman <dmytrofishman@gmail.com>
2015-05-12 23:14:54 +0300

    Code for generating test lmdb is added

--- Code/traincaffe/create_drdnet.sh
+++ Code/traincaffe/create_drdnet.sh
@@ -10,6 +10,7 @@ TOOLS=$HOME/Software/Caffe/build/tools
 
 TRAIN_DATA_ROOT=$IMAGES/$PREPROC/train
 VAL_DATA_ROOT=$IMAGES/$PREPROC/val
+TEST_DATA_ROOT=$IMAGES/$PREPROC/test
 
 # Set RESIZE=true to resize the images to 256x256. Leave as false if images have
 # already been resized using another tool.
@@ -36,6 +37,13 @@ if [ ! -d "$VAL_DATA_ROOT" ]; then
   exit 1
 fi
 
+if [ ! -d "$TEST_DATA_ROOT" ]; then
+  echo "Error: TEST_DATA_ROOT is not a path to a directory: $TEST_DATA_ROOT"
+  echo "Set the TEST_DATA_ROOT variable in create_imagenet.sh to the path" \
+       "where the ImageNet validation data is stored."
+  exit 1
+fi
+
 echo "Creating train lmdb..."
 
 GLOG_logtostderr=1 $TOOLS/convert_imageset \
@@ -55,5 +63,15 @@ GLOG_logtostderr=1 $TOOLS/convert_imageset \
     $VAL_DATA_ROOT/ \
     $CAFFEINPUT/$PREPROC/val.txt \
     $CAFFEINPUT/$PREPROC/ilsvrc12_val_lmdb
+
+echo "Creating test lmdb..."
+
+GLOG_logtostderr=1 $TOOLS/convert_imageset \
+    --resize_height=$RESIZE_HEIGHT \
+    --resize_width=$RESIZE_WIDTH \
+    --shuffle \
+    $TEST_DATA_ROOT/ \
+    $CAFFEINPUT/$PREPROC/test.txt \
+    $CAFFEINPUT/$PREPROC/ilsvrc12_test_lmdb
 
 echo "Done."
