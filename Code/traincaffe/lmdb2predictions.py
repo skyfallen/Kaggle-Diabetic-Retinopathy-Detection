720c8bf33e097f30dd65742787bc51b1b2d2d37b
kuz <ilya.kuzovkin@gmail.com>
2015-05-14 11:16:42 +0300

    The beginning of the lmdb-to-predictions conversion tool

---
+++ Code/traincaffe/lmdb2predictions.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+import sys
+import lmdb
+import numpy as np
+import caffe
+
+if len(sys.argv) < 3:
+  print 'Usage:', sys.argv[0], '<preprocessing_type> <model_name>'
+  print 'Usage example: python lmdb2predictions.py size256 basic'
+  sys.exit(2)
+
+preprocessing_type = sys.argv[1]
+model_name = sys.argv[2]
+features_lmdb_path = '/storage/hpc_anna/Kaggle_DRD/sample_features/' + preprocessing_type + '/features_' + model_name
+predictions_in_file = '/storage/hpc_anna/Kaggle_DRD/sample_caffeinput/test.txt'
+predictions_out_file = '/storage/hpc_anna/Kaggle_DRD/sample_predictions/' + preprocessing_type + '/predictions_' + model_name + '.txt'
+
+env = lmdb.open(features_lmdb_path)
+env_stat = env.stat()
+num_images = env_stat['entries']
+print "Number of images: ", num_images
+
+data = None
+with env.begin() as txn:
+    with txn.cursor() as curs:
+        for key, value in curs:
+
+            print key
+
+            # convert value to numpy array
+            datum = caffe.proto.caffe_pb2.Datum()
+            datum.ParseFromString(value)
+            arr = caffe.io.datum_to_array(datum)
+
+            # lazily initialize matrix, once we know number of features
+            if data is None:
+                num_features = arr.shape[0]
+                print "Number of features: ", num_features
+                data = np.empty((num_images, num_features))
+
+            # copy data to matrix
+            data[int(key), ] = arr[:, 0, 0]
+
+env.close()
+
+# read test.txt
+#with open(predictions_in_file, 'r') as f:
+	
+
+# store predictions to file
+#with open(predictions_file, 'w') as f:
+	
+
+#np.savez_compressed(npz_file, data=data, labels=labels)
+
+
+
+
+
