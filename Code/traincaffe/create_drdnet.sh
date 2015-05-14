818736a6d742330b2c787941be1592b4c4faa2ed
Dmytro Fishman <dmytrofishman@gmail.com>
2015-05-14 12:14:15 +0300

    shuffle option is excluded from create_drdnet.sh

--- Code/traincaffe/create_drdnet.sh
+++ Code/traincaffe/create_drdnet.sh
@@ -69,7 +69,6 @@ echo "Creating test lmdb..."
 GLOG_logtostderr=1 $TOOLS/convert_imageset \
     --resize_height=$RESIZE_HEIGHT \
     --resize_width=$RESIZE_WIDTH \
-    --shuffle \
     $TEST_DATA_ROOT/ \
     $CAFFEINPUT/test.txt \
     $CAFFEINPUT/$PREPROC/ilsvrc12_test_lmdb
