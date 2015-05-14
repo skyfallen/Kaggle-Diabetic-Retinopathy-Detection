59993a01ea7853d961dc21a5a220e4944b75ad60
kuz <ilya.kuzovkin@gmail.com>
2015-05-14 13:03:04 +0300

    Uncommon numbers of samples images

--- Code/preparedata/samplingtest.sh
+++ Code/preparedata/samplingtest.sh
@@ -2,6 +2,6 @@
 # Taking randomly images in order to create sample of a test set
 #
 
-ls  /storage/hpc_anna/Kaggle_DRD/images/raw/test |sort -R |tail -n 110 |while read file; do
+ls  /storage/hpc_anna/Kaggle_DRD/images/raw/test |sort -R |tail -n 107 |while read file; do
 	cp  /storage/hpc_anna/Kaggle_DRD/images/raw/test/$file /storage/hpc_anna/Kaggle_DRD/sample_images/raw/test
 done
--- Code/preparedata/samplingtrain.sh
+++ Code/preparedata/samplingtrain.sh
@@ -1,7 +1,7 @@
 #
 #  Taking randomly images in order to create sample of a training set 
 #
-ls  /storage/hpc_anna/Kaggle_DRD/images/raw/train |sort -R |tail -n 80 |while read file; do
+ls  /storage/hpc_anna/Kaggle_DRD/images/raw/train |sort -R |tail -n 71 |while read file; do
 	cp  /storage/hpc_anna/Kaggle_DRD/images/raw/train/$file /storage/hpc_anna/Kaggle_DRD/sample_images/raw/train
 	#echo "/storage/hpc_anna/Kaggle_DRD/images/raw/train/$file /storage/hpc_anna/Kaggle_DRD/sample_images/raw/train"
 done
