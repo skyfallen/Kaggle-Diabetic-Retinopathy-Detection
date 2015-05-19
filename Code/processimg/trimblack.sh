881d6da77e75169cb79568d169539f172200cdaf
kuz <ilya.kuzovkin@gmail.com>
2015-05-19 21:26:13 +0300

    Trimming script supplied with command line arguments

--- Code/processimg/trimblack.sh
+++ Code/processimg/trimblack.sh
@@ -2,11 +2,20 @@
 #
 # Deleting black area from all images
 #
+# Usage: ./trimblack.sh PREFIX SUBSET
+# Example: ./trimblack.sh "" train
+#
+
+PREFIX=$1
+SUBSET=$2
+
+echo "Running with PREFIX="$PREFIX" and SUBSET="$SUBSET
+read -n1 -r -p "Is it OK? (any key if yes, ^C if no)" key
 
 PREFIX=''
 
-INPUT="/storage/hpc_anna/Kaggle_DRD/"$PREFIX"images/raw/val"
-OUTPUT="/storage/hpc_anna/Kaggle_DRD/"$PREFIX"images/trimmed/val"
+INPUT="/storage/hpc_anna/Kaggle_DRD/"$PREFIX"images/raw/"$SUBSET
+OUTPUT="/storage/hpc_anna/Kaggle_DRD/"$PREFIX"images/trimmed/"$SUBSET
 
 find $INPUT -name "*.jpeg" -print0 | xargs -0 ls | parallel convert {} -fuzz 5% -trim $OUTPUT/{/.}.jpg
 
