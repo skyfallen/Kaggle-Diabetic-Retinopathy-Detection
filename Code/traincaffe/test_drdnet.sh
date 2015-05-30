612468d49558d269405f317039f77177ab699bf3
kuz <ilya.kuzovkin@gmail.com>
2015-05-30 17:21:11 +0300

    Store network descriptions in PREPROC/MODELNAME instead of just MODELNAME

--- Code/traincaffe/test_drdnet.sh
+++ Code/traincaffe/test_drdnet.sh
@@ -23,7 +23,7 @@ rm -rf "/storage/hpc_anna/Kaggle_DRD/"$PREFIX"features/"$PREPROC"/features_"$MOD
 # extract
 srun --partition=gpu --gres=gpu:1 --constraint=K20 --mem=16000 \
 $HOME"/Software/Caffe/build/tools/extract_features.bin" "/storage/hpc_anna/Kaggle_DRD/"$PREFIX"caffeinput/"$PREPROC"/model_"$MODELNAME"/model_iter_"$NITER".caffemodel" \
-$HOME"/Kaggle/Diabetic-Retinopathy-Detection/Code/traincaffe/networks/"$MODELNAME"/network_"$MODELNAME"_"$SUBSET".prototxt" \
+$HOME"/Kaggle/Diabetic-Retinopathy-Detection/Code/traincaffe/networks/"$PREPROC"/"$MODELNAME"/network_"$MODELNAME"_"$SUBSET".prototxt" \
 prob \
 "/storage/hpc_anna/Kaggle_DRD/"$PREFIX"features/"$PREPROC"/features_"$MODELNAME"_"$SUBSET $NSAMPLES lmdb GPU 0
 
