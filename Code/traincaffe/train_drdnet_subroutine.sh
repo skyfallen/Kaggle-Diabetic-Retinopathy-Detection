b425294e6c2bb7e7037470e9865864111bea9d70
kuz <ilya.kuzovkin@gmail.com>
2015-06-15 16:11:37 +0300

    bug: prefix missing from paths

--- Code/traincaffe/train_drdnet_subroutine.sh
+++ Code/traincaffe/train_drdnet_subroutine.sh
@@ -11,5 +11,5 @@ if [ "$PREFIX" == "EMPTY" ]; then
 	PREFIX=""
 fi
 
-srun --partition=gpu --gres=gpu:1 --constraint=K20 --mem=40000 $HOME"/Software/Caffe/build/tools/caffe" train --solver=$HOME"/Kaggle/Diabetic-Retinopathy-Detection/Code/traincaffe/networks/"$PREPROC"/"$MODELNAME"/network_"$MODELNAME"_solver.prototxt"
+srun --partition=gpu --gres=gpu:1 --constraint=K20 --mem=40000 $HOME"/Software/Caffe/build/tools/caffe" train --solver=$HOME"/Kaggle/Diabetic-Retinopathy-Detection/Code/traincaffe/networks/"$PREPROC"/"$PREFIX$MODELNAME"/network_"$MODELNAME"_solver.prototxt"
 
